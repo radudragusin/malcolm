2d513d16fcadf993a9077eab56d8950b4f5b24d9
malcolm <malcolm@paula-laptop>
2010-01-19 21:40:41 +0100

    Added comments.

--- MFSComFUSE.py
+++ MFSComFUSE.py
@@ -1,41 +1,22 @@
-#!/usr/bin/env python
-
-import time
-
 """
 Based on the examples provided with fusepy binding created by Giorgos Verigakis
 from http://code.google.com/p/fusepy/
 """
 
-"""
-errno.EACCES
-	Permission denied
-http://docs.python.org/library/errno.html?highlight=eacces#errno.EACCES
-"""
+import time
 from errno import EACCES
-
-"""
-os.path.realpath(path)
-    Return the canonical path of the specified filename, eliminating any symbolic links 
-    encountered in the path (if they are supported by the operating system).
-http://docs.python.org/library/os.path.html?highlight=realpath#os.path.realpath
-"""
 from os.path import realpath
-
 from sys import argv, exit
-
+from threading import Lock
 """
 threading.Lock()
     A factory function that returns a new primitive lock object. Once a thread has acquired 
     it, subsequent attempts to acquire it block, until it is released; any thread may release it.
 http://docs.python.org/library/threading.html?highlight=threading.lock#threading.Lock
 """
-from threading import Lock
 
 import os
-
 from fuse import FUSE, Operations, LoggingMixIn
-
 import MFSCore
 
 class MFSComFUSE(LoggingMixIn, Operations):    
@@ -46,185 +27,135 @@ class MFSComFUSE(LoggingMixIn, Operations):
     def __call__(self, op, path, *args):
         return super(MFSComFUSE, self).__call__(op, self.root + path, *args)
     
-    """ uses the access() system call to verify permissions over path.
-    where mode:
-		os.F_OK Value to pass as the mode parameter of access() to test the existence of path.
-		os.R_OK Value to include in the mode parameter of access() to test the readability of path.
-		os.W_OK Value to include in the mode parameter of access() to test the writability of path.
-		os.X_OK Value to include in the mode parameter of access() to determine if path can be executed.
-	http://docs.python.org/library/os.html?highlight=os.access#os.access
-    """
     def access(self, path, mode):
+        """ uses the access() system call to verify permissions over path.
+        http://docs.python.org/library/os.html?highlight=os.access#os.access
+        """
         if not os.access(path, mode):
             raise OSError(EACCES, '')
     
-    #http://docs.python.org/library/os.html#os.chmod
     chmod = os.chmod
-    #http://docs.python.org/library/os.html#os.chown
-    chown = os.chown
+    """http://docs.python.org/library/os.html#os.chmod"""
     
-    """
-    O_CREAT		If the file does not exist, it will be created. 
-    O_WRONLY	Write only flag.
-    http://docs.python.org/library/os.html#os.open
-    """
+    chown = os.chown
+    """#http://docs.python.org/library/os.html#os.chown"""
+    
     def create(self, path, mode):
+        """ O_CREAT		If the file does not exist, it will be created. 
+            O_WRONLY	Write only flag.
+        http://docs.python.org/library/os.html#os.open
+        """
         return os.open(path, os.O_WRONLY | os.O_CREAT, mode)
     
-    """
-    Force write of file with filedescriptor fd to disk.
-    http://docs.python.org/library/os.html?highlight=os.chmod#os.fsync
-    """
     def flush(self, path, fd):
+        """ Force write of file with file descriptor fd to disk.
+        http://docs.python.org/library/os.html?highlight=os.chmod#os.fsync
+        """
         return os.fsync(fd)
 
     def fsync(self, path, datasync, fd):
         return os.fsync(fd)
                 
-    """
-    os.lstat(path)
-		Like stat(), but do not follow symbolic links.
-		Perform a stat() system call on the given path. The return value is an object 
-		whose attributes correspond to the members of the stat structure, namely: 
-			st_mode (protection bits), 
-			st_ino (inode number), 
-			st_dev (device), 
-			st_nlink (number of hard links), 
-			st_uid (user id of owner), 
-			st_gid (group id of owner), 
-			st_size (size of file, in bytes), 
-			st_atime (time of most recent access), 
-			st_mtime (time of most recent content modification), 
-			st_ctime (platform dependent; time of most recent metadata change on Unix, or the time of creation on Windows)
-	http://docs.python.org/library/os.html#os.stat
-    """
     def getattr(self, path, fd=None):
+        """ os.lstat(path) Like stat(), but do not follow symbolic links.
+        http://docs.python.org/library/os.html#os.stat
+        """
         st = os.lstat(path)
         return dict((key, getattr(st, key)) for key in ('st_atime', 'st_ctime',
             'st_gid', 'st_mode', 'st_mtime', 'st_nlink', 'st_size', 'st_uid'))
     
-    """
-    We do not use extended attributes yet.
-    Extended attributes might be used to define versioning policies for a file.
-    http://manpages.ubuntu.com/manpages/karmic/en/man5/attr.5.html
-    http://manpages.ubuntu.com/manpages/karmic/en/man1/setfattr.1.html
-    """
     getxattr = None
+    """http://manpages.ubuntu.com/manpages/karmic/en/man5/attr.5.html and http://manpages.ubuntu.com/manpages/karmic/en/man1/setfattr.1.html"""
         
-    """
-    listxattr  retrieves  the  list  of extended attribute names associated 
-    with the given path in the filesystem.
-    """
     listxattr = None
+    """listxattr  retrieves  the  list  of extended attribute names associated with the given path in the filesystem."""
     
-    """
-    link(target, name)
-		Should create the filesystem object name as a hardlink to target.
-    http://docs.python.org/library/os.html#os.link
-    """
+    
     def link(self, target, source):
+        """link(target, name) Should create the filesystem object name as a hardlink to target.
+        http://docs.python.org/library/os.html#os.link
+        """
         return os.link(source, target)
     
-    #http://docs.python.org/library/os.html#os.mkdir
     mkdir = os.mkdir
-    #http://docs.python.org/library/os.html#os.mknod
+    """http://docs.python.org/library/os.html#os.mkdir"""
+    
     mknod = os.mknod
-    #http://docs.python.org/library/os.html#os.open
+    """http://docs.python.org/library/os.html#os.mknod"""
+    
     open = os.open
+    """http://docs.python.org/library/os.html#os.open"""
         
-    """
-    os.lseek(fd, offset, how)
-		Set the current position of file descriptor fd to position offset, modified by how: 
-		SEEK_SET or 0 to set the position relative to the beginning of the file; 
-		SEEK_CUR or 1 to set it relative to the current position; 
-		os.SEEK_END or 2 to set it relative to the end of the file.
-    http://docs.python.org/library/os.html#os.lseek
-    os.read(fd, n)
-		Read at most n bytes from file descriptor fd. Return a string containing the bytes 
-		read. If the end of the file referred to by fd has been reached, an empty string is 
-		returned.
-	http://docs.python.org/library/os.html#os.read
-    """
     def read(self, path, size, offset, fd):
+        """ os.lseek(fd, offset, how) Set the current position of file descriptor fd to position offset, 
+        modified by how (0 to set the position relative to the beginning of the file). 
+        http://docs.python.org/library/os.html#os.lseek
+        os.read(fd, n) Read at most n bytes from file descriptor fd.
+        http://docs.python.org/library/os.html#os.read
+        Code here executes with Lock held.  The lock is guaranteed to be released when the block is left. 
+        http://www.python.org/dev/peps/pep-0343/
+        """
         with self.rwlock:
-			# Code here executes with Lock held.  The lock is guaranteed to be 
-			# released when the block is left. http://www.python.org/dev/peps/pep-0343/
             os.lseek(fd, offset, 0)
             return os.read(fd, size)
     
-    """
-    os.listdir(path)
-		Return a list containing the names of the entries in the directory given by path. 
-		The list is in arbitrary order. It does not include the special entries '.' and 
-		'..' even if they are present in the directory.
-	http://docs.python.org/library/os.html#os.listdir
-    """
     def readdir(self, path, fd):
+        """ os.listdir(path) Return a list containing the names of the entries in the directory given by path. 
+        The list is in arbitrary order. It does not include the special entries '.' and 
+        '..' even if they are present in the directory.
+        http://docs.python.org/library/os.html#os.listdir
+        """
         return ['.', '..'] + os.listdir(path)
 
-    #http://docs.python.org/library/os.html#os.readlink
     readlink = os.readlink
+    """http://docs.python.org/library/os.html#os.readlink"""
     
-    #http://docs.python.org/library/os.html#os.close
     def release(self, path, fd):
+        """http://docs.python.org/library/os.html#os.close"""
         return os.close(fd)
-        
-    #http://docs.python.org/library/os.html#os.rename
+    
     def rename(self, old, new):
+        """http://docs.python.org/library/os.html#os.rename"""
         return os.rename(old, self.root + new)
     
-    #http://docs.python.org/library/os.html#os.rmdir
     def rmdir(self, path):
+        """http://docs.python.org/library/os.html#os.rmdir"""
         MFSCore.rmdir(path)
     
-    """
-    The function statvfs() returns information about a mounted file system.
-               f_bsize;   file system block size 
-               f_frsize;  fragment size 
-               f_blocks;  size of fs in f_frsize units 
-               f_bfree;   # free blocks 
-               f_bavail;  # free blocks for non-root 
-               f_files;   # inodes 
-               f_ffree;   # free inodes 
-               f_favail;  # free inodes for non-root 
-               f_fsid;    file system ID 
-               f_flag;    mount flags 
-               f_namemax; maximum filename length 
-	http://manpages.ubuntu.com/manpages/lucid/en/man2/statvfs.2.html
-    http://docs.python.org/library/os.html#os.statvfs
-    """
     def statfs(self, path):
+        """ The function statvfs() returns information about a mounted file system.
+        http://manpages.ubuntu.com/manpages/lucid/en/man2/statvfs.2.html
+        http://docs.python.org/library/os.html#os.statvfs
+        """
         stv = os.statvfs(path)
         return dict((key, getattr(stv, key)) for key in ('f_bavail', 'f_bfree',
             'f_blocks', 'f_bsize', 'f_favail', 'f_ffree', 'f_files', 'f_flag',
             'f_frsize', 'f_namemax'))
     
-    #http://docs.python.org/library/os.html#os.symlink
     def symlink(self, target, source):
+        """http://docs.python.org/library/os.html#os.symlink"""
         return os.symlink(source, target)
     
     def truncate(self, path, length, fd=None):
+        """Uses a template for opening a file that ensures the file is closed when 
+        the block is left. http://www.python.org/dev/peps/pep-0343/"""
         with open(path, 'r+') as f:
-			#A template for opening a file that ensures the file is closed when 
-			#the block is left. http://www.python.org/dev/peps/pep-0343/
             MFSCore.truncate(path,length,f)
             f.truncate(length)
     
-    #Remove (delete) the file path. This is the same function as remove(); the unlink() 
-    #name is its traditional Unix name. http://docs.python.org/library/os.html#os.unlink
-    #http://docs.python.org/library/datetime.html?highlight=time#strftime-behavior
-    def unlink(self, path):	
+    def unlink(self, path):
 		MFSCore.unlink(path)
 	
-    #http://docs.python.org/library/os.html#os.utime
     utimens = os.utime
+    """http://docs.python.org/library/os.html#os.utime"""
     
-    #http://docs.python.org/library/os.html#os.write
     def write(self, path, data, offset, fd):
+        """http://docs.python.org/library/os.html#os.write"""
         with self.rwlock:
             MFSCore.write(path)         
             os.lseek(fd, offset, 0)
             return os.write(fd, data)
 
 def startFS(sourcePath,destPath,fg):
+    """Start the filesystem. """
     FUSE(MFSComFUSE(sourcePath),destPath,foreground = fg)
--- MFSCore.py
+++ MFSCore.py
@@ -4,6 +4,10 @@ import shutil
 import xattr
 import re
 
+
+"""The MFSConfig dictionary: 
+	This is the default configuration for the filesystem. This configuration will be used 
+	unless otherwise specified in the setting.MMFS file."""
 MFSConfig = dict([
 	('versioning_enabled', 'true'),
 	('max_no_versions', '-1'), 		#-1:disabled
@@ -13,11 +17,12 @@ MFSConfig = dict([
 	('mountpoint_dest', os.path.expanduser('~'))
 ])
 
+
 def rmdir(path):
 	if shouldVersion(path):
 		shutil.move(path, versionPath(path))
 	else:
-		shutil.rmtree(path) #os.rmdir(path)
+		shutil.rmtree(path)
 
 def truncate(path, length, f):
 	size = os.path.getsize(path)
@@ -43,12 +48,23 @@ def write(path):
 		os.close(fn)
 		os.close(ft)
 
-#build the path of the new version (format: .<filename>-<timestamp>.MFS)
 def versionPath(oldpath):
+	"""Returns a string containing the versioned path of the oldpath argument.
+	The new path is build based on the format: .<filename>-<timestamp>.MFS
+	"""
 	(d, b) = os.path.split(oldpath)
 	return d + os.sep + '.' + b + '-' + time.strftime(MFSConfig['timestamp_format']) + '.MFS'
 			
 def shouldVersion(path):
+	"""Returns True if the file (given as path) obeys the versioning restrictions, and False otherwise.
+	Returns True is all of the following file settings are satisfied:
+		it is not a version file (a file of format '.<filename>-<timestamp>.MFS')
+		the versioning_enabled attribute equals 'true'
+		the max_file_size attribute for the file is bigger than the actual file size
+		the max_no_versions attribute is bigger than the current number of versions of the file
+	The attributes are taken from the file's extended attributes. If an attribute cannot be found in the file's 
+	extended attribute, the filesystem's default value will be used.
+	"""
 	apath = os.path.join(MFSConfig['mountpoint_source'],os.path.relpath(str(path),MFSConfig['mountpoint_dest']))
 	apath = os.path.normpath(apath)
 	attrfile = xattr.xattr(apath)
@@ -65,6 +81,11 @@ def shouldVersion(path):
 	return True
 
 def isVersioningEnabled(attrfile):
+	"""Returns True if the file has versioning enabled (given by the 'versioning_enabled' setting of the file). False otherwise.
+	The value of the 'versioning_enabled' is taken from the extended attribute of the file, and if there is no such xattribute,
+	it uses the filesystem's default value.
+	Takes an xattr object (corresponding to a path) as argument.
+	"""
 	if 'user.versioning_enabled' in attrfile:
 		if attrfile.get('user.versioning_enabled') == 'true':
 			return True
@@ -77,6 +98,17 @@ def isVersioningEnabled(attrfile):
 			return False
 
 def isInRangeToVersion(attrfile,apath):
+	"""Returns True if the file has not yet reached the maximum number of versions permitted(given by the 'max_no_versions' 
+	setting of the file). Returns False otherwise.
+	Takes two arguments. The first is a xattr object corresponding to the path given as second argument.
+	The value of the 'max_no_versions' is taken from the extended attribute of the file, and if there is no such xattribute,
+	it uses the filesystem's default value.
+	A value of -1 for the 'max_no_versions' means that the number is unlimited (this is the default setting). 
+	A value of n>-1 means that a file can have only n versions, plus itself.
+	If the maximum number of versions has been reached, versioning will no longer take place, except in the event in which 
+	some of the older versions are manually deleted. [This behavior should be changed in future versions, so that the older 
+	versions are deleted when the maximum number has been reached, thus making space for the newest version]
+	"""
 	if 'user.max_no_versions' in attrfile:
 		if int(attrfile.get('user.max_no_versions')) == -1:
 			return True
@@ -89,17 +121,24 @@ def isInRangeToVersion(attrfile,apath):
 			return noOfVersions(apath) <= int(MFSConfig['max_no_versions'])
 
 def noOfVersions(apath):
+	"""Returns an integer representing the number of versions the file (given by argument apath) currently has.
+	A version file is a file of format '.<filename>-<timestamp>.MFS' 
+	It only searches in the current directory.
+	"""
 	if apath[-1] == os.sep:
 		(apath,file) = os.path.split(apath[1:-1])
 	else:
 		(apath,file) = os.path.split(apath)
 	files = os.listdir(apath)
-	r = re.compile("^\."+file+"-....-..-..-..-..-..\.MFS$") #version format: .<filename>-<timestamp>.MFS
+	r = re.compile("^\."+file+"-....-..-..-..-..-..\.MFS$")
 	versions = filter(r.search, files)
-	return len(versions)						
+	return len(versions)
 	
 import MFSComFUSE
 
 def startFS(sourcePath,destPath,fg=True):
+	"""Passes the filesystem start call to the MFSComFUSE module. 
+	It is called from module MFSAPI.
+	"""
 	MFSComFUSE.startFS(sourcePath, destPath, fg)
 	
